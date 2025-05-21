0f1c4fcdda246b45cfdbd11d2a2e90b08ab420ff
AITEHS <turetskiy5002@gmail.com>
2025-05-21 02:50:43 +0200

    created a couple of classes using vim. Exhausted

---
+++ main.py
@@ -0,0 +1,17 @@
+import tkinter
+
+class Date():
+    pass
+
+class GoalData():
+    def __init__(self,name:str, description:str, targetAmount:float, deadline:Date, category:str):
+        self.name = name
+        self.description = description
+        self.targetAmount = targetAmount
+        self.deadline = deadline
+        self.category = category
+
+
+    def validate():
+        pass
+
