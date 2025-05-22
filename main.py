2d8a8fc4512c9c0244dad59993a9390286b3cd46
AITEHS <turetskiy5002@gmail.com>
2025-05-23 01:01:29 +0200

    added some other classes

--- main.py
+++ main.py
@@ -1,10 +1,25 @@
 import tkinter
+#possibly to modify: GoalSettings\goalId = int, change id to goalID or something if it is already reserved for something python related, remove notifications,
+
+class GoalSettings():
+    def __init__(self, goalId:str,notificationsEnabled:bool, reminderFrequency:int, autoTracking:bool):
+        self.goalId = goalId
+        self.notificationsEnabled = notificationsEnabled
+        self.reminderFrequency = reminderFrequency 
+        self.autoTracking = autoTracking
+
+    def validate():
+        output:bool
+        pass
+
+    def applySettings():
+        pass
 
 class Date():
     pass
 
 class GoalData():
-    def __init__(self,name:str, description:str, targetAmount:float, deadline:Date, category:str):
+    def __init__(self, name:str, description:str, targetAmount:float, deadline:Date, category:str):
         self.name = name
         self.description = description
         self.targetAmount = targetAmount
@@ -13,5 +28,67 @@ class GoalData():
 
 
     def validate():
+        output:bool
+        pass
+
+class  GoalDetailsUI():
+    def __init__(self, goal:Goal):
+        self.goal = goal
+
+    def displayGoalInfo():
+        pass
+
+    def updateProgress(amount:float):
+        pass
+
+    def openGoalSettings():
+        pass
+
+
+class  GoalSettingsUI():
+    def __init__(self, goal:Goal):
+        self.goal = goal
+
+    def displaySettings():
+        pass
+
+    def modifySettings(settings:GoalSettings):
+        pass
+
+    def deleteGoalWithConfirmation():
+        pass
+
+    def showDeleteConfirmation():
+        output:bool
+        pass
+
+class Goal():
+    def __init__(self, id:str, name:str, description:str, targetAmount:float, currentAmount:float, deadline:Date, createdDate:Date, category:str, isCompleted:bool):
+        self.id = id
+        self.name = name
+        self.description = description
+        self.targetAmount = targetAmount
+        self.currentAmount = currentAmount
+        self.deadline = deadline
+        self.createdDate = createdDate
+        self.category = category
+        self.isCompleted = isCompleted
+
+    def updateProgress(amount:float):
+        pass
+
+    def claculateProgress():
+        output:float
+        pass
+
+    def isGoalAchieved():
+        output:bool
+        pass
+
+    def getSettings():
+        output:GoalSettings
+        pass
+
+    def updateSettings(settings:GoalSettings):
         pass
 
